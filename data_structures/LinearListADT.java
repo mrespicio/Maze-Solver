eb8329df14df1c2c95cd935616908153c8586bc0
Megan Respicio <34357105+mrespicio@users.noreply.github.com>
2018-02-25 19:59:06 -0800

    Update LinearListADT.java

--- data_structures/LinearListADT.java
+++ data_structures/LinearListADT.java
@@ -1,8 +1,3 @@
-/* 
-  Date: October 2017
-  Assignment prompt given. LinearList was created as an extension of this ADT
-*/
-
 package data_structures;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
